3168a99fd5be35b6275355cb5dafcd6f8731a94c
pgxtips <brandongill123@gmail.com>
2026-01-19 18:55:15 +0000

    update: figured out why the trees were not sitting

--- lua/plugin-config/treesitter.lua
+++ lua/plugin-config/treesitter.lua
@@ -1,9 +1,24 @@
 require("nvim-treesitter").setup({
-
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
     highlight = {
         enable = true,
     }
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    -- all languages servers installed need to be declared for some 
+    -- stupid ass reason, like bruh at least just have a flag that just does 
+    -- this for all installed parsers, do I look like a mug, am i joke to you?
+    pattern = { 'erlang', 'lua' },
+    callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- folds, provided by Neovim
+        --vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        --vim.wo.foldmethod = 'expr'
+        -- indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
 })
